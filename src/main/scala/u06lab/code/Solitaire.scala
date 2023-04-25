2a5308c70dfd5d9af498155f2c5815097c7544a8
『 Matteo Santoro 』 <megadigimatt@gmail.com>
2023-04-25 15:39:30 +0200

    * this is a bad solution, we can do better.....

--- src/main/scala/u06lab/code/Solitaire.scala
+++ src/main/scala/u06lab/code/Solitaire.scala
@@ -18,15 +18,15 @@ object Solitaire extends App:
       yield row.mkString
     rows.mkString("\n")
 
-def renderSolution(solution: Seq[(Int, Int)], width: Int, height: Int): String =
-  val reversed = solution.reverse
-  val rows =
-    for y <- 0 until height
-        row = for x <- 0 until width
-                  number = reversed.indexOf((x, y)) + 1
-        yield if number > 0 then "%-2d ".format(number) else "X  "
-    yield row.mkString
-  rows.mkString("\n")
+  def renderSolution(solution: (Seq[(Int, Int)], Int) , width: Int, height: Int): String =
+    val reversed = solution._1.reverse
+    val rows =
+      for y <- 0 until height
+          row = for x <- 0 until width
+                    number = reversed.indexOf((x, y)) + 1
+          yield if number > 0 then "%-2d ".format(number) else "X  "
+      yield row.mkString
+    rows.mkString("\n")
 
   def computeMoves(c : Card) =
     Set(
@@ -41,15 +41,19 @@ def renderSolution(solution: Seq[(Int, Int)], width: Int, height: Int): String =
 
   def placeMarks(width : Int = size, height : Int = size)(n : Int = deckSize)(using factory : IterableFactory) : Iterable[Solution] =
     n match
-      case 1 => factory(Set((width/2 , height/2)))
+      case 0 => factory(Set((width/2 , height/2)))
       case _ =>
         for
           placedCards <- placeMarks()(n - 1)
-          y <- 1 to size
-          card=(n, y)
+          y <- 0 to size
+          x <- 0 to size
+          card=(x, y)
 //          _ = Console.println("serie: %d \n computing: %s with res: %b".format(n, card, computeMoves(placedCards.last) filter(inBoardPosition()) contains(card)))
 //          _ = Console.println(render(placedCards.toSeq, size, size))
-//          _ = Console.println("\n")
+          _ = Console.println("\n")
+          _ = Console.println(placedCards)
+//            _ = Console.println(placedCards.last)
+//            _ = Console.println("\n")
           if computeMoves(placedCards.last) filter(inBoardPosition()) contains(card)
         yield
           placedCards.toSeq :+ card
@@ -58,8 +62,7 @@ def renderSolution(solution: Seq[(Int, Int)], width: Int, height: Int): String =
 //  println(render(solution = placeMarks()().view, width = 5, height = 5))
 
 //  placeMarks()() foreach()
+  placeMarks(3, 3)(9).zipWithIndex foreach((el, index) => println("for solution %d there are %d sol".format(index, el.size)))
 
-
-  val sol = placeMarks()()
-  println(sol.size)
+//  println(placeMarks()().size)
 
